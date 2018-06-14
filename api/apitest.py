e478b21b2333385c444b95c4cc43ade28f1d7815
Robert Fono <fonorobert@gmail.com>
2018-06-15 00:51:27 +0200

    small test of Flask-RESTful

---
+++ api/apitest.py
@@ -0,0 +1,22 @@
+from flask import Flask, request
+from flask_restful import Resource, Api
+
+app = Flask(__name__)
+api = Api(app)
+
+# define resources in classes
+
+class HelloWorld(Resource):
+    def get(self):
+        response = {"status": "ok"}
+        return response, 200
+    
+class Forbidden(Resource):
+    def get(self):
+        return {"error": "FORBIDDEN!"}, 403
+
+api.add_resource(HelloWorld, '/')
+api.add_resource(Forbidden, '/forbidden')
+
+if __name__ == '__main__':
+    app.run(debug=True)
